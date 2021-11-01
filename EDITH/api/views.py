0b47e45c646e57dd9ea9a2e36f3494883767fb06
gomader <gh970818@hotmail.com>
2021-11-02 00:17:31 +0900

    sketch api test

--- EDITH/api/views.py
+++ EDITH/api/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from EDITH.settings import STATIC_ROOT
+import os,cv2,base64
 
 # Create your views here.
 def sketch(request):
-    pass
+    #source_file = request.FILES["source"]
+    img_im = cv2.imread(os.path.join(STATIC_ROOT,"image/test.png"))
+    image_data = base64.b64encode(cv2.imencode('.png',img_im)[1]).decode()
+    return HttpResponse(image_data,content_type="image/png")
--- EDITH/static/script/PageScripts/index.js
+++ EDITH/static/script/PageScripts/index.js
@@ -3,7 +3,15 @@ $("#history_button").click(function(){
 })
 
 $("#generate_button").click(function(){
-	
+	$.ajax({
+		type: "POST",
+		url: "/api/sketch_api/",
+		data: "",
+		success: function(result){
+			console.log(result);
+			$("#sketch_canvas").wPaint("image", "data:image/png;base64,"+result);
+		}
+	})
 })
 
 $("#colorize_button").click(function(){
