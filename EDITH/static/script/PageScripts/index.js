48d4403a9981480b5793440ee741e0566cc728b0
gomader <gh970818@hotmail.com>
2021-11-01 15:00:53 +0900

    index page without functions

---
+++ EDITH/static/script/PageScripts/index.js
@@ -0,0 +1,15 @@
+$("#history_button").click(function(){
+	
+})
+
+$("#generate_button").click(function(){
+	
+})
+
+$("#colorize_button").click(function(){
+	
+})
+
+$("#complete_button").click(function(){
+	
+})
