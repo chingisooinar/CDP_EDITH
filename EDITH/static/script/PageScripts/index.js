8e89c19a8a477b96ba7ac9252c0603e25a3b1433
gomader <gh970818@hotmail.com>
2021-11-20 05:11:13 +0900

    without history

--- EDITH/static/script/PageScripts/index.js
+++ EDITH/static/script/PageScripts/index.js
@@ -21,3 +21,34 @@ $("#colorize_button").click(function(){
 $("#complete_button").click(function(){
 	
 })
+
+function tags(id){
+	console.log(id);
+}
+
+function showEidingState(){
+	$("#stateTitle").html("");
+	$("#editing_button").hide();
+	$("#generate_button").show();
+	$("#colorize_button").show();
+	$("#complete_button").show();
+	$("#download_button").hide();
+	$("#save_button").hide();
+	$(".tag_menu").show();
+}
+
+function showFinishState(){
+	$("#stateTitle").html("Final Product");
+	$("#editing_button").css("display","inline-block");
+	$("#generate_button").hide();
+	$("#colorize_button").hide();
+	$("#complete_button").hide();
+	$("#download_button").show();
+	$("#save_button").show();
+	$(".tag_menu").hide();
+}
+
+function showHistory(){
+	$("#stateTitle").html("My History");
+	$("#history_button").css("display","inline-block");
+}
