a37804c6b254b9ddd10fe8b04bff062f8119c739
gomader <gh970818@hotmail.com>
2021-11-22 23:23:15 +0900

    pages Layout

--- EDITH/homepage/views.py
+++ EDITH/homepage/views.py
@@ -5,7 +5,9 @@ def index(request):
     return render(request,'index.html',locals())
 	
 def result(request):
+	picUrl = "" # The result url
 	return render(request,'result.html',locals())
 	
 def history(request):
+	historyList = list() # The history list
 	return render(request,'history.html',locals())
---
+++ EDITH/static/script/PageScripts/result.js
@@ -0,0 +1,15 @@
+$("#edit_button").click(function(){
+	
+})
+
+$("#history_button").click(function(){
+	$(location).attr("href","/history/");
+})
+
+$("#download_button").click(function(){
+	
+})
+
+$("#saveas_button").click(function(){
+	
+})
