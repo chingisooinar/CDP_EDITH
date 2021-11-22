a0e3da95178de42b1e5b00908fc65c7d52e68314
soohunee <soohun96@g.skku.edu>
2021-11-22 20:05:03 +0900

    Integrate generate sketch api

--- EDITH/api/views.py
+++ EDITH/api/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from EDITH.settings import STATIC_ROOT
 import os,cv2,base64
+from api import AiModels
 
 # Create your views here.
 def sketch(request):
     #source_file = request.FILES["source"]
-    img_im = cv2.imread(os.path.join(STATIC_ROOT,"image/test.png"))
-    image_data = base64.b64encode(cv2.imencode('.png',img_im)[1]).decode()
-    return HttpResponse(image_data,content_type="image/png")
+    
+    response = AiModels.generateSketch('api/anime.jpg')
+    
+    
+    #img_im = cv2.imread(os.path.join(STATIC_ROOT,"image/test.png"))
+    #image_data = base64.b64encode(cv2.imencode('.png',response)[1]).decode()
+    return HttpResponse(response,content_type="image/png")
