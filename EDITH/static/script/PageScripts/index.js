737e2a6b2d15caf710ec8fa9d8e3a37b5b1db7d3
gomader <gh970818@hotmail.com>
2021-11-23 00:14:44 +0900

    api request functions

--- EDITH/static/script/PageScripts/index.js
+++ EDITH/static/script/PageScripts/index.js
@@ -3,11 +3,31 @@ $("#history_button").click(function(){
 })
 
 $("#generate_button").click(function(){
-
+	var imageData = $("#sketch_canvas").wPaint("image");
+	var fd = new FormData();
+	fd.append("image",imageData);
+	$.ajax({
+		url: '/api/sketch_api/',
+		data: fd,
+		type: "POST",
+		success: function(result){
+			$("#sketch_canvas").wPaint("image","data:image/png;base64,"+result);
+		}
+	})
 })
 
 $("#colorize_button").click(function(){
-	
+	var imageData = $("#sketch_canvas").wPaint("image");
+	var fd = new FormData();
+	fd.append("image",imageData);
+	$.ajax({
+		url: '/api/colorize_api/',
+		data: fd,
+		type: "POST",
+		success: function(result){
+			$("#sketch_canvas").wPaint("image","data:image/png;base64,"+result);
+		}
+	})
 })
 
 $("#complete_button").click(function(){
@@ -15,11 +35,31 @@ $("#complete_button").click(function(){
 })
 
 $("#convert_sketch_button").click(function(){
-	
+	var imageData = $("#sketch_canvas").wPaint("image");
+	var fd = new FormData();
+	fd.append("image",imageData);
+	$.ajax({
+		url: '/api/convert_to_sketch_api/',
+		data: fd,
+		type: "POST",
+		success: function(result){
+			$("#sketch_canvas").wPaint("image","data:image/png;base64,"+result);
+		}
+	})
 })
 
 $("#convert_bw_button").click(function(){
-	
+	var imageData = $("#sketch_canvas").wPaint("image");
+	var fd = new FormData();
+	fd.append("image",imageData);
+	$.ajax({
+		url: '/api/convert_to_bw_api/',
+		data: fd,
+		type: "POST",
+		success: function(result){
+			$("#sketch_canvas").wPaint("image","data:image/png;base64,"+result);
+		}
+	})
 })
 
 function tags(id){
