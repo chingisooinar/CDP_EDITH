d3438f24aae8870236ff64c580553265ba405ad3
gomader <gh970818@hotmail.com>
2021-12-02 00:39:39 +0900

    fix reedit bugs and download funtion

--- EDITH/static/script/PageScripts/result.js
+++ EDITH/static/script/PageScripts/result.js
@@ -1,5 +1,12 @@
 $("#edit_button").click(function(){
-	$(location).attr("href","/index/");
+	var form = $("<form method='post'></form>");
+	form.attr({"action":'/index/'});
+	var input = $("<input type='hidden'>");
+	input.attr({"name":"filename"});
+	input.val($.FILENAME);
+	form.append(input);
+	$(document.body).append(form);
+	form.submit();
 })
 
 $("#history_button").click(function(){
@@ -7,7 +14,9 @@ $("#history_button").click(function(){
 })
 
 $("#download_button").click(function(){
-	
+	var download = $("<a href='/media/user/"+ $.FILENAME +"' download='" + $.FILENAME + "'></a>");
+	$(document.body).append(download);
+	download[0].click();
 })
 
 $("#saveas_button").click(function(){
